fbfcc029800705352a660c5b890ba4cf49c4f6ca
Celine Ma <celine.ma@monash.edu>
2021-05-29 02:08:19 +1000

    add comment to solution

--- solution/solution.js
+++ solution/solution.js
@@ -8,7 +8,7 @@ const csvWriter = createArrayCsvWriter({
   path: "solution/out/data.csv",
 });
 
-var counter = {
+var counter = { //I choose to use object rather than other format so it would be easier to increment and make the code cleaner
   ACT: 0,
   NSW: 0,
   NT: 0,
@@ -19,15 +19,15 @@ var counter = {
   WA: 0,
 };
 
-data.default.forEach((e) => {
-  const currentState = e.address.split(", ")[1];
-  counter[currentState] += 1; //?
+data.default.forEach((e) => { //I was originally thinking about using array.map and write state into a new array and then count. but it will loop throug the whole array 8 times more, So I decide to increment straight away
+  const currentState = e.address.split(", ")[1]; //using this to avoid switch cases to make the code clean, split() is the easiest way that I can think of to extract the State
+  counter[currentState] += 1;
 });
 
-const result = Object.entries(counter);
+const result = Object.entries(counter); // reformat the counter so it can be used in csv-writer
 
-csvWriter
+csvWriter //copied from the csv-writer documentation, used the existing library so don't need to reinvent the wheel :)
   .writeRecords(result) // returns a promise
   .then(() => {
-    console.log("...Done");
+    console.log("restult has been written into CSV file");
   });
